7fa8fa738065e713f1e31bcf5014200bf2326d7f
加藤北斗 <hokuto15@gmail.com>
2018-10-16 16:17:45 +0900

    - babal-loader8対応 - モジュールアップデート

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 						options: {
 							cacheDirectory: true,
 							presets: [
-								['env', {'modules': false}]
+								['@babel/preset-env', {'modules': false}]
 							]
 						}
 					}
